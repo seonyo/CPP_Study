7377d29d48e2409f549aca6b800b9046c7594592
youngging <jsy6535@naver.com>
2023-03-18 18:51:15 +0900

    C++ 기본

---
+++ 2023 CPP/C++ 기본.cpp
@@ -0,0 +1,15 @@
+/ cout 객체 사용을 위해 씀
+#include <iostream>
+
+//namespace는 기억공간이라는 뜻. (변수 이름의 중복 방지)
+using namespace std;
+
+int main(void) {
+
+	int year = 2023;
+
+	//"<<"은 출력연산, C++은 객체로 출력한다
+	// endl : 개행 (줄바꿈)을 해주는 객체
+	cout << "hello world " << year << endl;
+	return 0;
+}
