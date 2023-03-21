a577597fe4a947598ccbecc8236d525eee0f5884
youngging <jsy6535@naver.com>
2023-03-21 13:49:48 +0900

    포인터

--- 2023 CPP/C++ 기본.cpp
+++ 2023 CPP/C++ 기본.cpp
@@ -1,4 +1,4 @@
-/ cout 객체 사용을 위해 씀
+// cout 객체 사용을 위해 씀
 #include <iostream>
 
 //namespace는 기억공간이라는 뜻. (변수 이름의 중복 방지)
@@ -9,7 +9,7 @@ int main(void) {
 	int year = 2023;
 
 	//"<<"은 출력연산, C++은 객체로 출력한다
-	// endl : 개행 (줄바꿈)을 해주는 객체
+	// endl : 개행 (줄바꿈)을 해주는 객체 (endline의 줄임말)
 	cout << "hello world " << year << endl;
 	return 0;
 }
---
+++ 2023 CPP/구구단.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void) {
+	for (int i = 2; i <= 9; i++) {
+		for (int j = 1; j <= 9; j++) {
+			printf("%d X %d = %d\n",i,  j, i * j);
+		}
+	}
+}
