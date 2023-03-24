f314457c1af7a12a27ae7ae29d6fdbf694b38a0b
seonyo <jsy6535@naver.com>
2023-03-24 09:15:29 +0900

    C++스타일 문자열복사

--- 2023 CPP/문자열 복사.cpp
+++ 2023 CPP/문자열 복사.cpp
@@ -1,14 +1,15 @@
-﻿#include <stdio.h>
-#include <string.h>
+﻿#include <iostream>
+#include <string>	  // str::string 클래스
 
 int main(void) {
-	char str1[30] = "";
-	char str2[30] = "Jiwoo";
+	std::string str1;
+	std::string  str2="Jiwoo";
 
-	// str1에 str2의 내용을 집어넣겠다
-	strcpy(str1, str2);
+	// str1에 str2의 내용을 집어넣겠다. 첫번째 인자가 목적지!
+	str1 = str2;
 	
-	printf("%s \n", str1);
+	std::cout << str1 << std::endl;
 
 	return 0;
+
 }
