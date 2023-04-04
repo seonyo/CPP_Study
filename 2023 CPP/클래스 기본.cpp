1a04464ab9594035c4f4b87ad24c16eeb63a95a1
seonyo <jsy6535@naver.com>
2023-04-04 14:28:25 +0900

    매개변수가 있는 생성자

--- 2023 CPP/클래스 기본.cpp
+++ 2023 CPP/클래스 기본.cpp
@@ -7,8 +7,9 @@ using namespace std;
 //class는 디폴트가 private (struct는 디폴트가 public)
 class Student {
 
-
 public:
+	//생성자 (constructor) : 객체를 생성할 때 호출되는 함수
+	//생성자를 정의하지 않으면 default로 매개변수가 없는 생성자가 정의된다
 	Student()
 	{
 		grade = 2200;
@@ -17,8 +18,14 @@ public:
 		major = "뉴미디어소프트웨어과";
 		adress = "서울시 관악구";
 	}
-	//생성자 (constructor) : 객체를 생성할 때 호출되는 함수
-	//생성자를 정의하지 않으면 default로 매개변수가 없는 생성자가 정의된다
+
+	Student(int grade, string name, string phonenumber, string major, string adress) {
+		this->grade = grade;
+		this->name = name;
+		this->phonenumber = phonenumber;
+		this->major = major;
+		this->adress = adress;
+	}
 
 	void print(void) {
 		//C언어에서의 구조체는 함수 선언을 못하지만 c++은 가능
@@ -40,10 +47,17 @@ private:
 	string adress;
 
 };
-int main(void) {
-	//자료형 : Student (class 생략가능)
-	class Student information; 
 
-	information.print();
+int main(void) {
+	cout << "매개변수가 없는 클래스" << endl;
+	//자료형 : Student (class 생략가능)
+	//class Student stu1; 
+	Student stu1 = Student();
+	//stu1.print();
+	cout << endl << "매개변수가 있는 클래스" << endl;
+
+	//class Student stu2 (2216, "정선영", "010-6601-6535", "뉴미디어소프트웨어과", "서울시 송파구");
+	Student stu2 = Student(2216, "정선영", "010-6601-6535", "뉴미디어소프트웨어과", "서울시 송파구");
+	stu2.print();
 
 }
