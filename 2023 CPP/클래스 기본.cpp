e6c5edd0574a7afdd837a218b6e3870f9d956fbb
seonyo <jsy6535@naver.com>
2023-04-11 13:41:51 +0900

    FEAT: 정적할당과 동적할당 설명

--- 2023 CPP/클래스 기본.cpp
+++ 2023 CPP/클래스 기본.cpp
@@ -54,10 +54,11 @@ int main(void) {
 	Student stu1 = Student();
 	//stu1.print();
 
-	//정적할당
+	//정적할당 : 컴파일 시간에 메모리 크기가 결정
 	//Student stu2 = Student(2216, "정선영", "010-6601-6535", "뉴미디어소프트웨어과", "서울시 송파구");
 	//stu2.print();
 	
+	//동적할당 : 실행시간(runtime)에 메모리 크기가 결정 (메모리; heap영역)
 	//동적할당 된 공간은 포인터로 접근한다.
 	Student* stu3 = new Student(22, "율곡", "010-0000-0000", "유교", "한성");
 	stu3->print();
