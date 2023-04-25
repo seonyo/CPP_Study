94dcf54183b076fe31f8cfa67aaa85c09435609e
seonyo <jsy6535@naver.com>
2023-04-25 13:37:33 +0900

    소멸자로 생성자에서 동적할당한 메모리 해재

--- 2023 CPP/클래스 중급.cpp
+++ 2023 CPP/클래스 중급.cpp
@@ -7,13 +7,20 @@ class MString {
 public :
 	MString(const char* str)
 	{
-		unsigned int l = strlen(str);
+		unsigned int l = strlen(str); //양수만 들어갈때 unsigned를 쓴다.
 		c_str_ = new char[l + 1]; //'\0'(널문자)가 들어갈 공간 +1
 		strcpy(c_str_, str);
 		size_ = l;
 	}
+	// 소멸자(destructor)
+	// 소멸자는 매개변수가 없다
+	~MString()
+	{
+		//소멸자로 생성자에서 동적할당한 메모리 해제
+		delete[] c_str_;
+	}
 	unsigned int size(void) { return size_; }
-	 char* c_str(void) { return c_str_; }
+	char* c_str(void) { return c_str_; }
 private:
 	unsigned int size_; //문자열의 길이
 	 char* c_str_;      //문자열의 시작주소
@@ -27,7 +34,6 @@ int main(void) {
 
 	//객체 str만 삭제. 동적할당으로 생성된 문자열은 그대로 남아있음(메모리 누수)
 	//TODO : 소멸자(destructor)를 이용하여 동적할당된 메모리도 해제하자
-
 	delete str;
 	return 0;
 }
