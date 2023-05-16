c1b70b5decae1d4e4e20eeaeea812dbcfab87ef1
seonyo <jsy6535@naver.com>
2023-05-16 14:35:50 +0900

    복사생성자에도 깊은복사

--- 2023 CPP/복사생성자.cpp
+++ 2023 CPP/복사생성자.cpp
@@ -4,16 +4,33 @@ using namespace std;
 
 class Munja {
 public:
+	//일반생성자
 	Munja(const char* jhqz)
 	{
 		str_ = new char[strlen(jhqz)+1];
 		strcpy(str_, jhqz);
 		cout << "일반생성자 호출" << endl;
 	}
+	
+	//복사생성자(얕은복사)
+	Munja(const Munja& hj) : str_(hj.str_)
+	{
+		str_ = new char[strlen(hj.str_) + 1];
+		strcpy(str_, hj.str_);
+		cout << "복사생성자 호출" << endl;
+	}
+	~Munja()
+	{
+		delete[] str_;
+		cout << "소멸자 호출" << endl;
+	}
+
 private:
 	char* str_;
 };
+
 int main(void) {
-	Munja m1 = Munja("abc");
+	Munja m1 = Munja("abc");  //일반생성자 호출
+	Munja m2 = m1; //복사생성자 호출
 	return 0;
 }
