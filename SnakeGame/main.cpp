923291cd09ff44e8fd1e2b973af19008a034f13d
seonyo <jsy6535@naver.com>
2023-05-19 10:05:02 +0900

    [SFML] 창 띄우기

---
+++ SnakeGame/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+int main(void) {
+	RenderWindow window(VideoMode(640, 480), "Snake Game"); //  프레임 보이게
+	
+	while (window.isOpen())
+	{
+		Event e;
+		while (window.pollEvent(e)) {
+			if (e.type == Event::Closed)
+				window.close();
+		}
+		window.clear();
+		window.display();
+	}
+	return 0;
+}
