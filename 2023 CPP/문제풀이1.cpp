8aeaf8584b0d894b4f40bd36ddd111caf9b13d10
seonyo <jsy6535@naver.com>
2023-05-26 09:33:47 +0900

    깊은복사 생성자

---
+++ 2023 CPP/문제풀이1.cpp
@@ -0,0 +1,37 @@
+class IntArray {
+
+public:
+	IntArray(int* arr, int size) {
+		arr_ = new int[size];
+		size_ = size;
+
+		for (int i = 0; i < size_; i++) {
+			arr_[i] = arr[i];
+		}
+	}
+
+	//깊은 복사생성자
+	IntArray(const IntArray& rhs) 
+		{
+		size_ = rhs.size_;
+		arr_ - new int[size_];
+		for (int i = 0; i < size_; i++) {
+			arr_[i] = rhs.arr_[i];
+		}
+	}
+
+
+private :
+	int* arr_;
+	int size_;
+	
+
+};
+
+int main(void) {
+	int arr[] = { 1,2,3 };
+	IntArray ia = IntArray(arr, sizeof(arr)/sizeof(*arr));
+	IntArray ca = ia;  //복사생성자 호출
+
+	return 0;
+}
