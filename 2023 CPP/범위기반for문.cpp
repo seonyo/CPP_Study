e112a3987a1fc8ae2c978ed1a54e87378287b629
seonyo <jsy6535@naver.com>
2023-10-17 14:30:28 +0900

    범위기반 for문

--- 2023 CPP/범위기반for문.cpp
+++ 2023 CPP/범위기반for문.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main(void) {
 	int arr[] = {13,31,65,47,53};
 
-	for (int i = 0; i < 5; i++) {
-		cout << arr[i] << endl;
+	for (int x : arr) {
+		cout << x << endl;
 	}
 
 	return 0;
