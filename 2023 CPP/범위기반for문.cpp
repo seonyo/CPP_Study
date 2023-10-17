b9b6989b704b6fc774b2de12e9515c98c7790139
seonyo <jsy6535@naver.com>
2023-10-17 14:35:42 +0900

    auto를 통해 자료형으로 자동으로 맞춰준다

--- 2023 CPP/범위기반for문.cpp
+++ 2023 CPP/범위기반for문.cpp
@@ -5,9 +5,13 @@ using namespace std;
 int main(void) {
 	int arr[] = {13,31,65,47,53};
 
-	for (int x : arr) {
+	//누가봐도 a의 자료형은 int이다
+	auto a = 4;
+
+	//x의 자료형은 dobule로 자동설정
+	for (auto x : arr) {
 		cout << x << endl;
 	}
-
+	
 	return 0;
 }
